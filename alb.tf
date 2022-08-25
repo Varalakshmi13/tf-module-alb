74d3126e020d3b58af7ea75cffbc03f9784d7300
Varalakshmi G <varalakshmi.ganjam@gmail.com>
2022-08-25 21:46:44 +0530

    Updating tf-module-alb

--- alb.tf
+++ alb.tf
@@ -3,17 +3,9 @@ resource "aws_lb" "alb" {
   internal           = var.INTERNAL
   load_balancer_type = "application"
   security_groups    = var.INTERNAL ? [aws_security_group.alb_private.id] : [aws_security_group.alb_public.id]
-  subnets            = var.INTERNAL ? 
-
-  enable_deletion_protection = true
-
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.bucket
-    prefix  = "test-lb"
-    enabled = true
-  }
+  subnets            = var.INTERNAL ? data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS : data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_IDS
 
   tags = {
-    Environment = "production"
+    Name = var.ALB_NAME
   }
 }
