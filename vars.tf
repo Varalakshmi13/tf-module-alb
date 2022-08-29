a14bcc116429f5a3d03f30e723f507bf72ada0b9
Varalakshmi G <varalakshmi.ganjam@gmail.com>
2022-08-29 06:47:39 +0530

    Updating tf-module-alb

--- vars.tf
+++ vars.tf
@@ -2,6 +2,6 @@ variable "ENV" {}
 variable "ALB_NAME" {}
 variable "INTERNAL" {}
 
-variable "ALB_ARN" {}
-variable "LISTNER_ARN" {}
-variable "ALB_ADDRESS" {}
+//variable "ALB_ARN" {}
+//variable "LISTNER_ARN" {}
+//variable "ALB_ADDRESS" {}
